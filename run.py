69ea8de518dcab7fadafc4dc9276ef637c12477a
Gareth Aubrey <aubreygareth@gmail.com>
2024-08-25 12:04:47 +0000

    Add while true to get_actual_data function

--- run.py
+++ run.py
@@ -12,19 +12,30 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Work_hours')
 
+#function for getting data input from user
+
 def get_actual_data():
     """
     Get hours worked (actual) data fom user
     """
-    print("Please enter number of hours worked by each employee.")
-    print("Data should be 10 numbers, seperated by commas")
-    print("Example: 12,8,6,0,0,0,12,8,6,0\n")
+    while True:
+        print("Please enter number of hours worked by each employee.")
+        print("Data should be 10 numbers, seperated by commas")
+        print("Example: 12,8,6,0,0,0,12,8,6,0\n")
 
-    hours_worked = input("Enter hours here:")
-    print(f"Hours provided are {hours_worked}")
+        hours_worked = input("Enter hours here:")
+        print(f"Hours provided are {hours_worked}")
 
-    actual_data = hours_worked.split(",")
-    validate_data(actual_data)
+        actual_data = hours_worked.split(",")
+        validate_data(actual_data)
+
+        if validate_data(actual_data):
+            print("Data is valid!")
+            break
+
+    return actual_data
+
+#function for validating data from user
 
 def validate_data(values):
     """
@@ -33,11 +44,15 @@ def validate_data(values):
     or if there isn't the correct amount of values (10)
     """
     try:
+        [float(value) for value in values]
         if len(values) != 10:
             raise ValueError(
                 f"Exactly 10 values required, you provided {len(values)}"
             )
     except ValueError as e:
-        print(f"invalid data: {e}, please try again.\n")        
+        print(f"invalid data: {e}, please try again.\n")     
+        return False
 
-get_actual_data()
+    return True       
+
+data = get_actual_data()
